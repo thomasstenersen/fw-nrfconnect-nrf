c0d028236f63c7feaed1eada4dd7ff16def38c36
omku <Omkar.Kulkarni@nordicsemi.no>
2019-02-01 10:58:51 +0100

    DRGN-10904: Add lock files.

---
+++ subsys/bluetooth/controller/lock.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include "lock.h"
+
+#include <soc.h>
+#include <gpio.h>
+
+/* Semaphore with a single count functioning as a lock. */
+K_SEM_DEFINE(ble_controller_lock, 1, 1);
+
+int api_lock(void)
+{
+	//printk("locking\n");
+#if defined(CONFIG_BLECTLR_SHARED_API_BLOCKING)
+#if defined(CONFIG_BLECTLR_SHARED_API_BLOCKING_WITH_TIMEOUT)
+	return k_sem_take(&ble_controller_lock,
+			  CONFIG_BLECTLR_API_BLOCKING_TIMEOUT_VALUE);
+#elif defined(CONFIG_BLECTLR_SHARED_API_BLOCKING_FOREVER)
+	return k_sem_take(&ble_controller_lock, K_FOREVER);
+#else
+	return k_sem_take(&ble_controller_lock, K_FOREVER);
+#endif
+#else
+	return k_sem_take(&ble_controller_lock, K_NO_WAIT);
+#endif
+}
+
+int api_try_lock(void)
+{
+	return k_sem_take(&ble_controller_lock, K_NO_WAIT);
+}
+
+void api_unlock(void)
+{
+	k_sem_give(&ble_controller_lock);
+}
---
+++ subsys/bluetooth/controller/lock.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**
+ * @lock.h
+ *
+ * @brief This file defines APIs needed to lock the BLE controller APIs for threadsafe operation.
+ */
+
+#ifndef LOCK_H__
+#define LOCK_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <zephyr.h>
+#include <soc.h>
+#include <gpio.h>
+
+/** Macro representation for unlock API */
+#define API_UNLOCK api_unlock
+
+/** Macro representation for typical usage of locking API */
+#define API_LOCK_AND_RETURN_ON_FAIL(err)                           \
+	do {                                                           \
+		err = api_lock();                                     \
+		if (err != 0) {                                            \
+			return err;                                            \
+		}                                                          \
+	} while (0);
+
+/** @brief Try to take the lock while maintaining the specified blocking behavior.
+ *
+ * This API call will be blocked forever if @ref CONFIG_BLECTLR_SHARED_API_BLOCKING and
+ * @ref CONFIG_BLECTLR_SHARED_API_BLOCKING_FOREVER options are selected. Otherwise API call
+ * will wait for timeout with a given timeout value specified by
+ * @ref CONFIG_BLECTLR_SHARED_API_BLOCKING_WITH_TIMEOUT and return error if lock cannot be
+ * obtained. If @ref CONFIG_BLECTLR_SHARED_API_BLOCKING is not specified, API will return
+ * immediately in the event of not being able to obtain a lock.
+ *
+ * @retval 0 				Success
+ * @retval - ::NRF_EBUSY 	Returned without waiting.
+ * @retval - ::NRF_EAGAIN   Waiting period timed out.
+ */
+int api_lock(void);
+
+/** @brief Try to take the lock and return immediately on failure.
+ *
+ * This API is useful for use cases where waiting is not desirable (e.g. calling library API
+ * from IRQ).
+ *
+ * @retval 0 				Success
+ * @retval - ::NRF_EBUSY 	Returned without waiting.
+ */
+int api_try_lock(void);
+
+/** @brief Unlock the lock.
+ *
+ * @note This API is must be called only after lock is obtained. Otherwise it will block
+ * execution for forever.
+ */
+void api_unlock(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LOCK_H__ */
