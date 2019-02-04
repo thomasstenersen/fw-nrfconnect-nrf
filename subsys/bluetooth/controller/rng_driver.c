61ca8b3cabe70ae0f4725fb5227e14efd9430350
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2019-02-04 14:10:51 +0100

    Use RNG priority config for RNG driver
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- subsys/bluetooth/controller/rng_driver.c
+++ subsys/bluetooth/controller/rng_driver.c
@@ -104,7 +104,7 @@ static int rng_driver_init(struct device *dev)
 
 	k_sem_init(&rng_data.sem_sync, 0, 1);
 
-	IRQ_CONNECT(NRF5_IRQ_RNG_IRQn, 4, rng_driver_isr, NULL, 0);
+	IRQ_CONNECT(NRF5_IRQ_RNG_IRQn, CONFIG_ENTROPY_NRF5_PRI, rng_driver_isr, NULL, 0);
 
 	return 0;
 }
