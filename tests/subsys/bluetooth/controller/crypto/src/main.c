72b4d75cdcefb7105c9eb8386bfecb30d33e244e
omku <Omkar.Kulkarni@nordicsemi.no>
2019-02-12 13:53:20 +0100

    DRGN-11426: Update and simplify unit test.

--- tests/subsys/bluetooth/controller/crypto/src/main.c
+++ tests/subsys/bluetooth/controller/crypto/src/main.c
@@ -12,6 +12,7 @@
 
 #include "crypto.h"
 #include "ble_controller_soc.h"
+#include "mock_ext.h"
 
 #define LOG_MODULE_NAME tests_crypto
 LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
@@ -24,14 +25,8 @@ void test_bt_rand(void)
 
 	LOG_INF("bt_rand() @ 0x%08x\n", (uintptr_t) bt_rand);
 
-	/* Test invalid params */
-	zassert_equal(bt_rand(NULL, sizeof(rand_num)), -NRF_EINVAL, "Failed");
-	zassert_equal(bt_rand(rand_num, UINT8_MAX + 1), -NRF_EINVAL, "Failed");
-
 	/* Test function call */
-	ztest_expect_value(ble_controller_rand_vector_get, p_dst, rand_num);
-	ztest_expect_value(ble_controller_rand_vector_get, length, sizeof(rand_num));
-	zassert_equal(bt_rand(rand_num, sizeof(rand_num)), 5, "Failed");
+	zassert_equal(bt_rand(rand_num, sizeof(rand_num)), 0, "Failed");
 }
 
 void test_bt_encrypt_le(void)
@@ -44,16 +39,15 @@ void test_bt_encrypt_le(void)
 	memset(cleartext_in, 'C', KEYSIZE);
 	memset(ciphertext_out, 0, KEYSIZE);
 
-	ztest_expect_value(ble_controller_ecb_block_encrypt, key, key_in);
-	ztest_expect_value(ble_controller_ecb_block_encrypt, cleartext, cleartext_in);
-	ztest_expect_value(ble_controller_ecb_block_encrypt, ciphertext, ciphertext_out);
+	mock_expect(ble_controller_ecb_block_encrypt);
 	ztest_returns_value(ble_controller_ecb_block_encrypt, 0);
 	zassert_equal(bt_encrypt_le(key_in, cleartext_in, ciphertext_out), 0, "Failed");
 
 	/* Test custom mock behavior, this ensures that API's internal transformation on inputs (if any)
 	   works correctly. */
 	for (u8_t i = 0; i < KEYSIZE; i++) {
-		zassert_equal(cleartext_in[i], ciphertext_out[i], "Failed");
+		//printk("check: c (%d) = k (%d) ^ cl (%d)\n", ciphertext_out[i], key_in[i], cleartext_in[i]);
+		zassert_equal(ciphertext_out[i], key_in[i] ^ cleartext_in[i], "Failed");
 	}
 }
 
@@ -67,16 +61,15 @@ void test_bt_encrypt_be(void)
 	memset(cleartext_in, 'C', KEYSIZE);
 	memset(ciphertext_out, 0, KEYSIZE);
 
-	ztest_ignore_param(ble_controller_ecb_block_encrypt, key);
-	ztest_ignore_param(ble_controller_ecb_block_encrypt, cleartext);
-	ztest_ignore_param(ble_controller_ecb_block_encrypt, ciphertext);
+	mock_expect(ble_controller_ecb_block_encrypt);
 	ztest_returns_value(ble_controller_ecb_block_encrypt, 0);
 	zassert_equal(bt_encrypt_be(key_in, cleartext_in, ciphertext_out), 0, "Failed");
 
 	/* Test custom mock behavior, this ensures that API's internal transformation on inputs (if any)
 	   works correctly. */
 	for (u8_t i = 0; i < KEYSIZE; i++) {
-		zassert_equal(cleartext_in[i], ciphertext_out[i], "Failed");
+		//printk("check: c (%d) = k (%d) ^ cl (%d)\n", ciphertext_out[i], key_in[i], cleartext_in[i]);
+		zassert_equal(ciphertext_out[i], key_in[i] ^ cleartext_in[i], "Failed");
 	}
 }
 
